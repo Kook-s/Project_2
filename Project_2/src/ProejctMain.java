5eb5eae65c4e596aa74e1e842075f07cc9e4efe3
Kooks <kooks1126@naver.com>
2023-03-23 19:29:33 +0900

    good

--- Project_2/src/ProejctMain.java
+++ Project_2/src/ProejctMain.java
@@ -3,7 +3,10 @@ public class ProejctMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("테스트 라인");
+		System.out.println("테스트 라인");
+		System.out.println("테스트 라인");
+		System.out.println("테스트 라인");
+		System.out.println("테스트 라인");
 	}
 
 }
